d3518706f53217dd5c854e8f248afcf49308e22b
Farid Vakili <fvakili@cpp.edu>
2022-10-20 20:33:17 -0700

    Initial commit. Outline of what i think the program will look like.

---
+++ guessMain.c
@@ -0,0 +1,56 @@
+/* git reminders
+git remote add origin https://github.com/justFreds/Number_Guessing_Game.git
+git branch -M main
+git push -u origin main
+*/
+
+//PROMPT: INTERACTIVE MENU
+//                          Press 1 To Play Game
+//                          Press 2 To Change Max Number
+//                          Press 3 To Quit
+
+//maybe like in promptMenu.c example from class?
+//  CREATE A PROMPT MENU LIKE FILE
+//  promptGame.c
+//int getLine()
+//void menuDisplay()
+//int menuChoice()
+        //1: goes to the function with guessing game between (1-10)
+        //2: goes to the function with guessing game between (1-UserDefinedMax)
+        //3: Exits
+//void menuProcess()
+
+
+//FUNCTIONS
+//CREATE gameFunctions.c
+//  defaultGame()
+//  customGame()
+//  exit()
+
+
+//MAIN FUNCTION
+//
+//CALLS menuProcess()
+
+
+
+
+//RANDOMIZE THE MENU ITEM SELECTED
+
+//PASS THE TAX AND TIP INTO FUNCTION [RETURNS TOTAL]
+
+//DISPLAY: MENU ITEM CHOSEN
+//              COST
+//              TIP
+//              TAX
+//              TIP
+//              TOTAL
+
+
+
+
+/*
+    FUNCTION TO CALCULATE TOTAL [cost, tip, tax]
+    This is where we'll be calculating the cost
+    Total
+*/
