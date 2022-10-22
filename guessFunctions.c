f5d82507edc52c98ac9d37ff2f73181ea335985d
Farid Vakili <fvakili@cpp.edu>
2022-10-21 17:47:30 -0700

    Finished implemented the file read and write. Program Works =]

--- guessFunctions.c
+++ guessFunctions.c
@@ -21,7 +21,7 @@ void defaultGame(void) {
     //declarations
     const int DEFAULTMAX = 10;
     int secretNumber, userGuess;
-    char exitTracker, sentinal[20];
+    char sentinal[20];
     
     srand(time(NULL));
     secretNumber = rand()%DEFAULTMAX+1;     //RANDOM NUMBER FROM 1-10
@@ -72,7 +72,7 @@ void customGame(void) {
     printf("\n----Custom Game Mode----\n");
     //declarations
     int secretNumber, userGuess, customMax;
-    char exitTracker, sentinal[20];
+    char sentinal[20];
 
     //GET customMax from user
     printf("Enter The Custom Max Number To Guess (1-%d): ", RAND_MAX);
@@ -138,15 +138,59 @@ void customGame(void) {
 void loadGame(void) {
     
     printf("\n----Save File Loaded----\n");
-    
+    //declarations
+    int secretNumber, userGuess, customMax;
+    char sentinal[20];
     FILE *filePtr;
-    int customMax;
 
     filePtr = fopen("saveFile.txt", "r");        //READ MODE
     fscanf(filePtr, "%d\n", &customMax);    
     fclose(filePtr);
 
     printf("Custom Max: %d\n", customMax);
+
+    srand(time(NULL));
+    secretNumber = rand()%customMax+1;     //RANDOM NUMBER FROM 1-customMax
+
+    printf("A number between 1-%d has been generated.\n", customMax);
+    printf("Press Q/q at any time to return to previous menu.");
+    
+    GUESS(sentinal);
+    if(SENT_CHECK(sentinal))        //RETURNS TO MENU
+        goto skip;
+    userGuess = atoi(sentinal);    
+    
+    //MAIN LOOP
+    while(userGuess != secretNumber) {
+
+        while (userGuess <= 0 || userGuess > customMax) {       //CHECK BOUNDS
+            printf("\nERROR!\nNumber Out Of Bounds!\n");
+            GUESS(sentinal);
+            userGuess = atoi(sentinal);
+            if(SENT_CHECK(sentinal))
+                goto skip;          //RETURNS TO MENU
+        }                
+        if(userGuess < secretNumber) {                  //HINTS
+            printf("Guess is low. Try again.");
+        }
+        else {
+            printf("Guess is high. Try again.");
+        }
+        GUESS(sentinal);                                //REPEAT
+        if(SENT_CHECK(sentinal))
+            goto skip;              //RETURNS TO MENU
+        userGuess = atoi(sentinal);
+    }
+
+    if(userGuess == secretNumber) {                     
+        printf("\n\nCongrats! You guessed %d correctly!\nReturning to Main Menu now!\n", userGuess);
+        fflush(stdin);
+    }
+    else {
+        skip:
+        printf("\n\nThe random number was: %d\nReturning to Main Menu now!\n", secretNumber);
+        fflush(stdin);
+    }
 
 }
 //  exit()
